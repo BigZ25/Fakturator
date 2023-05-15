4d470f5e665caa06d740c538c253d6f1b529a106
BigZ25 <dominikzieba25@gmail.com>
2023-05-15 17:56:58 +0200

    next commit Mon May 15 17:56:58     2023

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,7 +4,7 @@
             <div class="col-span-2">
                 <div class="flex flex-col h-full">
                     <div class="bg-gray-900 h-12">
-                        <p class="text-center text-2xl">FAKTURATOR</p>
+                        <a href="{{route('home')}}"><p class="text-center text-2xl">FAKTURATOR</p></a>
                     </div>
                     <div class="bg-gray-900 h-full">
                         <ul class="list-none hover:list-disc">
