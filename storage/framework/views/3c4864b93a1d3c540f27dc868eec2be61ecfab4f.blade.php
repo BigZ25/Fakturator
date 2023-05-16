4a60414c77379e19c9be3a30872d419a096544de
BigZ25 <dominikzieba25@gmail.com>
2023-05-16 12:57:07 +0200

    next commit Tue May 16 12:57:07     2023

--- storage/framework/views/3c4864b93a1d3c540f27dc868eec2be61ecfab4f.blade.php
+++
@@ -1,7 +0,0 @@
-<?php extract(collect($attributes->getAttributes())->mapWithKeys(function ($value, $key) { return [Illuminate\Support\Str::camel(str_replace([':', '.'], ' ', $key)) => $value]; })->all(), EXTR_SKIP); ?>
-@props(['title','rounded','cardClasses','shadow','padding'])
-<x-card :title="$title" :rounded="$rounded" :card-classes="$cardClasses" :shadow="$shadow" :padding="$padding" {{ $attributes }}>
-<x-slot name="action" >{{ $action }}</x-slot>
-<x-slot name="footer" >{{ $footer }}</x-slot>
-{{ $slot ?? "" }}
-</x-card>
--- storage/framework/views/de2f99ac8360781162b75de552608d941d4ff3ba.php
+++
@@ -1,6 +0,0 @@
-<div class="mt-1" style="font-size: 12px;">
-    <hr class="mb-1">
-    <p><b>Data i czas utworzenia: </b><?php echo e($entity->created_at); ?></p>
-    <p><b>Data i czas modyfikacji: </b><?php echo e($entity->updated_at); ?></p>
-</div>
-<?php /**PATH C:\laragon\www\fakturator\resources\views/templates/show/timestamp.blade.php ENDPATH**/ ?>
