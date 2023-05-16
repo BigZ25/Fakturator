0cc5e65e7ae1fdb3adf023d6d72d90dbdd4a6605
BigZ25 <dominikzieba25@gmail.com>
2023-05-16 12:26:41 +0200

    next commit Tue May 16 12:26:41     2023

--- storage/framework/views/c64438a5e3c9353c76d3032c066e95176039ffec.php
+++
@@ -1,7 +0,0 @@
-<?php if($value): ?>
-    <div>
-        <p><b><?php echo e($label); ?>:</b></p>
-        <p><?php echo e($value); ?></p>
-    </div>
-<?php endif; ?>
-<?php /**PATH C:\laragon\www\fakturator\resources\views/templates/show/textarea.blade.php ENDPATH**/ ?>
--- storage/framework/views/dde0efe241a49135349d5786d429b8a31f730551.php
+++
@@ -1,8 +0,0 @@
-<?php if(isset($multiple)): ?>
-    <?php $__currentLoopData = $values; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $value): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <input multiple type="<?php echo e($type ?? 'text'); ?>" name=<?php echo e($name); ?> value="<?php echo e($value); ?>" style="display: none">
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-<?php else: ?>
-    <input type="<?php echo e($type ?? 'text'); ?>" name=<?php echo e($name); ?> value="<?php echo e($value); ?>" style="display: none">
-<?php endif; ?>
-<?php /**PATH C:\laragon\www\fakturator\resources\views/templates/form/hidden.blade.php ENDPATH**/ ?>
