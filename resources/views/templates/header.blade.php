1985ac22c5fa133e5d050242b1cf5a65f84edd81
BigZ25 <dominikzieba25@gmail.com>
2023-05-16 17:17:48 +0200

    next commit Tue May 16 17:17:48     2023

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,12 +1,12 @@
 @if(isset($title))
-    <div class="flex items-end">
+    <div class="flex items-start">
         <div class="pb-3" style="width: 50%">
             <h1 class="text-2xl">{{$title}}</h1>
             @if(isset($breadcrumbs) && $breadcrumbs)
                 <h1 class="mt-1"><a class="hover:text-primary-500" href="{{$breadcrumbs['route']}}"><u>{{$breadcrumbs['label']}}</u></a></h1>
             @endif
         </div>
-        <div class="pb-3" style="width: 50%; text-align: right">
+        <div class="pb-3 mt-2" style="width: 50%; text-align: right">
             <a href="{{route('settings.form')}}"><i class="fas fa-fw fa-user mr-1"></i>{{auth()->user()->email}}</a>
         </div>
     </div>
--- storage/framework/views/81b35a52c1259239e50fb3ca535ef98c6162647f.php
+++ storage/framework/views/81b35a52c1259239e50fb3ca535ef98c6162647f.php
@@ -1,12 +1,12 @@
 <?php if(isset($title)): ?>
-    <div class="flex items-end">
+    <div class="flex items-start">
         <div class="pb-3" style="width: 50%">
             <h1 class="text-2xl"><?php echo e($title); ?></h1>
             <?php if(isset($breadcrumbs) && $breadcrumbs): ?>
                 <h1 class="mt-1"><a class="hover:text-primary-500" href="<?php echo e($breadcrumbs['route']); ?>"><u><?php echo e($breadcrumbs['label']); ?></u></a></h1>
             <?php endif; ?>
         </div>
-        <div class="pb-3" style="width: 50%; text-align: right">
+        <div class="pb-3 mt-2" style="width: 50%; text-align: right">
             <a href="<?php echo e(route('settings.form')); ?>"><i class="fas fa-fw fa-user mr-1"></i><?php echo e(auth()->user()->email); ?></a>
         </div>
     </div>
