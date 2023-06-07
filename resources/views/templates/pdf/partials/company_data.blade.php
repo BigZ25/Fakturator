913fd7c3f0e2d46e8faac6a7c0dbe79188dab73c
BigZ25 <dominikzieba25@gmail.com>
2023-06-07 10:41:27 +0200

    next commit Wed Jun  7 10:41:26     2023

--- resources/views/templates/pdf/partials/company_data.blade.php
+++ resources/views/templates/pdf/partials/company_data.blade.php
@@ -8,7 +8,7 @@
         <tr>
             <td style="padding:10px;">
                 <h4>{{$invoice[$prefix.'_name']}}</h4>
-                <h4>NIP: {{$invoice[$prefix.'seller_nip']}}</h4>
+                <h4>NIP: {{$invoice[$prefix.'_nip']}}</h4>
                 <h4>{{$invoice[$prefix.'_address']}}</h4>
                 <h4>{{$invoice[$prefix.'_postcode']}} {{$invoice[$prefix.'_city']}}</h4>
             </td>
